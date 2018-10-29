aa08e0f8d7ecf1423e686ba75df906543a73800d
Ashwini Kumar <kumarashwini@outlook.com>
2018-10-29 16:36:47 +0530

    dev access logging support drafted

--- src/registry/domain/dev-access-details.js
+++ src/registry/domain/dev-access-details.js
@@ -1,8 +1,13 @@
 'use strict';
 
 module.exports = (conf, cdn) => {
-  // TODO (ashwkum4) - Add code to rotate the file log
-  const filePath = () => `${conf.s3.componentsDir}/update.log`;
+  const filePath = () => {
+    const d = new Date();
+    // Expected fileNameSuffix is `YEAR_QUARTER` e.g 2018-01 for duration Jan 2018 to Mar 2018
+    const fileNameSuffix = `${d.getUTCFullYear()}_0${Math.floor(d.getUTCMonth() / 3) + 1}`;
+    const fileName = `update-${fileNameSuffix}.log`;
+    return `${conf.s3.componentsDir}/${fileName}`;
+  };
   const getFile = callback => cdn.getFile(filePath(), true, callback);
   const save = (data, callback) =>
     cdn.putFileContent(data, filePath(), true, callback);
